d9f73e8341c4f86d752680d7ef115bd7a5653af1
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-02-20 12:11:59 +0530

    login complete without JWT

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -15,3 +15,26 @@ export const registerUser = async (req,res) =>{
         res.status(500).json({message:"User registration failed",error:error.message});
     }
 }
+
+
+
+//Login User || SIGN IN User
+
+export const loginUser = async (req,res) =>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if(!user){
+           return res.status(404).json({message:"User not found"});
+        }
+        const passwordMatch = await bcrypt.compare(password,user.password);
+        if(!passwordMatch){
+            return res.status(401).json({message:"Invalid password"});
+        }
+        res.status(200).json({message:"User Login successfully",data:user});
+
+        
+    } catch (error) {
+        res.status(500).json({message:"User Login failed",error:error.message});
+    }
+}
